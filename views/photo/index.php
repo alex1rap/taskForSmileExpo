19b521ec2a7e2197ea1f6b39e437e1a7fd2c9713
alex1rap <alex1rap@spaces.ru>
2019-05-19 21:46:12 +0300

    Task-1: Fixed nav links and translation

--- views/photo/index.php
+++ views/photo/index.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Create Photos'), ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Create Photo'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
